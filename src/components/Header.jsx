493d60dff76d4c443c4b8de2f501f890a77f9a5e
seodoha <doha.seo89@gmail.com>
2024-07-17 14:12:31 +0900

    finish

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ export default function Header() {
   log('<Header /> rendered', 1);
 
   return (
-    <header id="main-header">
-      <img src={logoImg} alt="Magnifying glass analyzing a document" />
+    <header id='main-header'>
+      <img src={logoImg} alt='Magnifying glass analyzing a document' />
       <h1>React - Behind The Scenes</h1>
     </header>
   );
